18419ece86a36b21f46a207acb296be5e8d026df
Talha <talhaaliarslan@gmail.com>
2014-09-17 17:13:55 +0200

    previous version files removed

--- matlab-src/skew.m
+++
@@ -1,7 +0,0 @@
-function [ X ] = skew( x )
-%SKEW Function to generate a 3X3 Skew Symmetric matrix out of a 3X1 vector.
-%   Detailed explanation goes here
-    X=[0 -x(3) x(2) ; x(3) 0 -x(1) ; -x(2) x(1) 0 ];
-
-end
-
