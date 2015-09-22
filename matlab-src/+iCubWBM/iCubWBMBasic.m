c47f66903530cebedcc1cbf713a05e061e69958b
Martin Neururer <martin.neururer@gmail.com>
2015-09-22 11:07:36 +0200

    First version of the iCubWBMBasic. Changed some parts in the UML.

--- matlab-src/+iCubWBM/iCubWBMBasic.m
+++ matlab-src/+iCubWBM/iCubWBMBasic.m
@@ -2,7 +2,11 @@ classdef iCubWBMBasic < handle & matlab.mixin.Copyable
     properties(Access = private)
         wb_params@iCubWBMParams
     end
-
+    
+    properties(Access = private, Constant)
+        wb_strArgError = 'Wrong number of input arguments!';
+    end
+    
     methods(Access = public)
         % Constructor:
         function obj = iCubWBMBasic(icub_wbm_params)
@@ -13,7 +17,7 @@ classdef iCubWBMBasic < handle & matlab.mixin.Copyable
         function newObj = copy(obj)
             try
                 % Matlab-tuning: try to use directly the memory (faster)
-                % note: works only for R2010b or newer.
+                % note: this works only for R2010b or newer.
                 objByteArray = getByteStreamFromArray(obj);
                 newObj = getArrayFromByteStream(objByteArray);                
             catch
@@ -42,7 +46,7 @@ classdef iCubWBMBasic < handle & matlab.mixin.Copyable
         
         function setState(q_j, dq_j, v_wb)            
             if (nargin ~= 3)
-                error('iCubWBMBasic::setState: Wrong number of input arguments!');
+                error('iCubWBMBasic::setState: %s', wb_strArgError);
             end 
             wholeBodyModel('update-state', q_j, dq_j, v_wb);
         end
@@ -54,51 +58,115 @@ classdef iCubWBMBasic < handle & matlab.mixin.Copyable
         
         function setWorldFrame(R_rootlnk_wf, p_rootlnk_wf, g_wf)
             if (nargin ~= 3)
-                error('iCubWBMBasic::setWorldFrame: Wrong number of input arguments!');
+                error('iCubWBMBasic::setWorldFrame: %s', wb_strArgError);
             end
             R_rootlnk_wf_arr = reshape(R_rootlnk_wf, [], 1); % reshape the matrix to an 1-column array ...
             wholeBodyModel('set-world-frame', R_rootlnk_wf_arr, p_rootlnk_wf, g_wf);
         end
         
-        function setWorldLink()
-            
+        function setWorldLink(urdf_link_name, R_reflnk_wf, p_reflnk_wf, g_wf)
+            R_reflnk_wf_arr = reshape(R_reflnk_wf, [], 1);
+
+            switch nargin
+                case 4
+                    wholeBodyModel('set-world-link', urdf_link_name, R_reflnk_wf_arr, p_reflnk_wf, g_wf); % check arguments here!!!
+                case 2
+                    wholeBodyModel('set-world-link', urdf_link_name, R_reflnk_wf_arr); % check arguments here!!!
+                otherwise
+                    error('iCubWBMBasic::setWorldLink: %s', wb_strArgError);
+            end
         end
         
-        function massMatrix()
-            
+        function M = massMatrix(q_j)
+            if ~exist('q_j', 'var')
+                M = wholeBodyModel('mass-matrix');
+                return
+            end
+            M = wholeBodyModel('mass-matrix', q_j);
         end
         
-        function jointLimits()
-            
+        function [jl_lower, jl_upper] = getJointLimits(varargin)
+            [jl_lower, jl_upper] = wholeBodyModel('joint-limits');
         end
         
-        function jacobian()
-            
+        function J = jacobian(urdf_link_name, q_j)
+            switch nargin
+                case 2
+                    J = wholeBodyModel('jacobian', urdf_link_name, q_j);                    
+                case 1
+                    J = wholeBodyModel('jacobian', urdf_link_name);
+                otherwise
+                    error('iCubWBMBasic::jacobian: %s', wb_strArgError);
+            end
+        end     
+                        
+        function djdq = dJdq(urdf_link_name, q_j, dq_j, v_xb)
+            switch nargin
+                case 4
+                    djdq = wholeBodyModel('djdq', urdf_link_name, q_j, dq_j, v_xb);
+                case 1
+                    djdq = wholeBodyModel('djdq', urdf_link_name);
+                otherwise
+                    error('iCubWBMBasic::dJdq: %s', wb_strArgError);
+            end
         end
         
-        function dJdq()
-            
+        function H = centrodialMomentum(q_j, dq_j, v_xb)
+            switch nargin
+                case 3
+                    H = wholeBodyModel('centroidal-momentum', q_j, dq_j, v_xb);
+                case 0 
+                    H = wholeBodyModel('centroidal-momentum');
+                otherwise
+                    error('iCubWBMBasic::centrodialMomentum: %s', wb_strArgError);
+            end
         end
         
-        function centrodialMomentum()
-            
+        function p = forwardKinematics(urdf_link_name, q_j)
+            switch nargin
+                case 2
+                    p = wholeBodyModel('forward-kinematics', urdf_link_name, q_j);
+                case 1
+                    p = wholeBodyModel('forward-kinematics', urdf_link_name);
+                otherwise
+                    error('iCubWBMBasic::forwardKinematics: %s', wb_strArgError);                    
+            end            
         end
         
-        function forwardKinematics()
-            
+        function C_qv = genBiasForces(q_j, dq_j, v_xb)
+            switch nargin
+                case 3
+                    C_qv = wholeBodyModel('generalised-forces',);
+                case 0
+                    C_qv = wholeBodyModel('generalised-forces', q_j, dq_j, v_xb);
+                otherwise
+                    error('iCubWBMBasic::genBiasForces: %s', wb_strArgError);
+            end
+        end       
+  
+        function wbm_params = getWBMParams(obj)
+            wbm_params = obj.wb_params;
         end
         
-        function genBiasForces()
-            
-        end
-        
-        function getWBMParams()
-            
-        end
-        
-        function showWBMParams(obj)
-            sprintf(1, ['iCub-parameters:\n robot (URDF): %s\n' ' NDOF: %i\n' ...
-                        'link name (frame): %s\n'], obj.urdfName, obj.ndof, obj.linkName);
+        function dispWBMParams(obj, precision)
+            if ~exist('precision', 'var')
+                precision = 2;
+            end
+            strParams = sprintf(['iCub-WBM parameters:\n\n'
+                                 ' URDF robot name:     %s\n' ...
+                                 ' URDF ref. link name: %s\n\n' ...
+                                 ' R (root link to world frame):\n\n %s\n\n' ...
+                                 ' p (root link to world frame):\n\n %s\n\n' ...
+                                 ' R (ref. link to world frame):\n\n %s\n\n' ...
+                                 ' p (ref. link to world frame):\n\n %s\n\n' ...
+                                 ' g (world frame):\n\n %s\n\n'], ...
+                                obj.wb_params.urdfRobotName, obj.wb_params.urdfLinkName, ...
+                                mat2str(obj.wb_params.R_rootlnk_wf, precision), ...
+                                mat2str(obj.wb_params.p_rootlnk_wf, precision), ...
+                                mat2str(obj.wb_params.R_reflnk_wf, precision), ...
+                                mat2str(obj.wb_params.p_reflnk_wf, precision), ...
+                                mat2str(obj.wb_params.g_wf, precision));
+           disp(strParams);
         end
 
     end
