97ebeef4ff36f2123f3f41a5b7d9bde166fdf697
Silvio Traversaro <silvio.traversaro@iit.it>
2015-09-26 17:00:41 +0200

    Add test infrastructure
    Now the test are running in a systematic way by simply typing
    ctest (or also make test if you are running make). For now
    only the base link test has been ported, while the conservation
    of kinematic energy test is broken (see issue
    https://github.com/robotology-playground/mex-wholebodymodel/issues/30).
    In the meanwhile also the FindMatlab.cmake was ported to the newest version
    available from upstream CMake.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-Project to create a mex C/C++ interface to the WBI WholeBodyModel components in order to be able to create forward dynamics on matlab for prototyping purposes.
+Project to create a mex C/C++ interface to the WBI WholeBodyModel components
+in order to be able to create forward dynamics on matlab for prototyping purposes.
 
+## Tests
+To verify regression in the code when you modify the code, some
+regression tests have been implemented in this repository.
+
+### Run tests
+To run the tests on this repository, just compile it and from the build directory launch
+the `ctest` command. If you want to print out the verbose output of the tests, launch `ctest -VV`.
+
+### Add new tests
+To add new tests, just write a script that runs your test and raise and exception
+if the test fails (for example using the `assert` command). Then modify the
+[tests/runWBMTests.m](tests/runWBMTests.m) file to call your testing script.
--- matlab-src/baseLinkFrameTest.m
+++ matlab-src/baseLinkFrameTest.m
@@ -30,7 +30,7 @@ if( initCond == 1)
     params.qjInit = [params.torsoInit;params.leftArmInit;params.rightArmInit;params.leftLegInit;params.rightLegInit] * (pi/180);
 else
     % random pose within joint limits
-    load('./jointLimits.mat');
+    jointLimits;
     params.qjInit = jl1 + rand(25,1).*(jl2-jl1);
 end
 
@@ -56,15 +56,21 @@ fprintf('Converting to a set world frame... \n');
 
 wbm_setWorldFrame(rot,pos,[ 0,0,-9.81]');
 
-[qj,T_b,dqj,vb] = wbm_getState();
+[qj,T_b_Got,dqj,vb] = wbm_getState();
                 
-[pos,rot] = frame2posrot(T_b);
+[posGot,rotGot] = frame2posrot(T_b_Got);
 fprintf('Post convertion rotation \n');
-disp(rot);
+disp(rotGot);
+WBMAssertEqual(posGot,pos);
+WBMAssertEqual(rotGot,rot);
 fprintf('Post conversion rotation check (R^T*R)\n');
-disp(rot'*rot);
+disp(rotGot'*rotGot);
+WBMAssertEqual(rotGot'*rotGot,eye(3,3));
 fprintf('Post conversion frame \n');
-disp(T_b');
+disp(T_b_Got');
+WBMAssertEqual(T_b_Got,T_b);
 fprintf('Post conversion quaternion norm \n');
-disp(norm(T_b(4:end)));
+disp(norm(T_b_Got(4:end)));
+WBMAssertEqual(norm(T_b_Got(4:end)),1);
+
                 
