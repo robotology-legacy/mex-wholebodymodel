a6a879d6256e9cb7a0e247e56adeb22bba10ae40
Gabriele <gabriele.nava@mail.polimi.it>
2015-10-23 15:44:01 +0200

    correct Rf documentation

--- matlab-src/mex-wholebodymodel_balancing/Rf.m
+++ matlab-src/mex-wholebodymodel_balancing/Rf.m
@@ -1,6 +1,9 @@
 function R = Rf(u)
+% Rf calculates the rotation matrix from a quaternion (real followed by imaginary definition)
+% R = Rf(u) returns a 3x3 valid rotation matrix from a given quaternion u (4x1), where u(1) is 
+% the scalar parameter and u(2:4) is the vector parameter in Rodriguez
+% formula
 
-%calculate the rotation matrix from a quaternion (real followed by imaginary definition)
 qt_b_mod_s = u(1);
 qt_b_mod_r = u(2:4);
 R          = eye(3) + 2*qt_b_mod_s*skew(qt_b_mod_r) + 2*skew(qt_b_mod_r)^2;
