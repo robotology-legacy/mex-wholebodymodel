1fa15d771d0aa26d2b7e7f6e2e1dd886aaa27e2d
Silvio Traversaro <silvio.traversaro@iit.it>
2015-10-23 16:21:58 +0200

    Remove spurious debug print

--- src/modeljointlimits.cpp
+++ src/modeljointlimits.cpp
@@ -65,7 +65,9 @@ bool ModelJointLimits::allocateReturnSpace(int nlhs, mxArray* plhs[])
 
 ModelJointLimits::~ModelJointLimits()
 {
+#ifdef DEBUG
   mexPrintf("ModelJointLimits destructed\n");
+#endif
 }
 
 ModelJointLimits* ModelJointLimits::getInstance()
