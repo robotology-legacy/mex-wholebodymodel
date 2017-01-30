a06bfbadc3c58bd5844dc0f4ad856125b467bee4
Gabriele <gabriele.nava@mail.polimi.it>
2017-01-30 10:31:43 +0100

    deleted old functions

--- mex-wholebodymodel/matlab/utilities/old/fromSkew2Vector.m
+++
@@ -1,9 +0,0 @@
-function  v  = fromSkew2Vector(S)
-%fromSkew2Vector Function to generate a 3X1 vector from a 3x3 Skew Symmetric matrix.
-
-    v    = zeros(3,1);
-    v(1) = -S(2,3);
-    v(2) =  S(1,3);
-    v(3) = -S(1,2);
-end
-
